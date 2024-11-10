637827aa5fd6b530ad67e69ce23aacb243902070
Max Shabs <97040079+maxshabs@users.noreply.github.com>
2024-11-10 10:39:50 +0200

    Update numberofsegmentsinastring_434.py

--- Easy/Strings/numberofsegmentsinastring_434.py
+++ Easy/Strings/numberofsegmentsinastring_434.py
@@ -1,3 +1,5 @@
+# Solution for LeetCode problem 434: Number of Segments in a String
+# Time Complexity: O(n), where n is the length of the input string s
 class Solution(object):
     def countSegments(self, s):
         """
