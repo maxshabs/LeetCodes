ed7ae1d64ca7f51e1349d8a1224cd8b514b761aa
Max Shabs <97040079+maxshabs@users.noreply.github.com>
2024-11-10 10:38:32 +0200

    Problem 482

---
+++ Easy/Strings/licensekeyformatting_482.py
@@ -0,0 +1,13 @@
+# Solution for LeetCode problem 482: License Key Formatting
+# Time Complecity: O(n), where n is the length of the input string s
+class Solution(object):
+    def licenseKeyFormatting(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        s = (s[::-1].replace("-", "")).upper()
+        substrings = [s[i: i + k][::-1] for i in range (0, len(s), k)]
+        return "-".join(substrings[::-1])
+        
