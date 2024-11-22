a41c032fbe152ec9b22c64f8df56c8b078d64c98
Max Shabs <97040079+maxshabs@users.noreply.github.com>
2024-11-22 23:15:44 +0200

    Update movezeros_283.py

--- Easy/Two Pointers/movezeros_283.py
+++ Easy/Two Pointers/movezeros_283.py
@@ -6,19 +6,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        zero_ptr = 0
         non_zero_ptr = 0
-        len_of_nums = len(nums)
-        for i in range(len_of_nums):
-            if nums[zero_ptr] != 0:
-                zero_ptr += 1
-                if zero_ptr >= len_of_nums:
-                    break
-            if nums[non_zero_ptr] == 0 or zero_ptr > non_zero_ptr:
+        nums_len = len(nums)
+        for i in range(nums_len):
+            if nums[i] != 0:
+                nums[non_zero_ptr], nums[i] = nums[i], nums[non_zero_ptr]
                 non_zero_ptr += 1
-                if non_zero_ptr >= len_of_nums:
-                    break
-            if nums[zero_ptr] == 0 and nums[non_zero_ptr] != 0 and zero_ptr < non_zero_ptr:
-                temp = nums[zero_ptr]
-                nums[zero_ptr] = nums[non_zero_ptr]
-                nums[non_zero_ptr] = temp
