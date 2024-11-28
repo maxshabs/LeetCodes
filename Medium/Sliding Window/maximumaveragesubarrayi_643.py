1831377dcea33ca952bd3340b539e5c084fa9ddd
Max Shabs <97040079+maxshabs@users.noreply.github.com>
2024-11-29 00:06:16 +0200

    Add files via upload

--- Medium/Sliding Window/maximumaveragesubarrayi_643.py
+++ Medium/Sliding Window/maximumaveragesubarrayi_643.py
@@ -1,13 +1,12 @@
+# Solution for LeetCode Problem 643. Maximum Average Subarray I
+# Time Complexity: O(n) where n is the size of the input array "nums"
+# Space Complexity: O(1) because we only store 2 variables
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        averages = []
-        cur_avg = 0
-        for i in range(k):
-            cur_avg += nums[i] / k
-        averages.append(cur_avg)
+        cur_avg = max_avg = sum(nums[:k])
         loop_len = len(nums) - (k - 1)
         for i in range(1, loop_len):
-            cur_avg -= nums[i - 1] / k
-            cur_avg += nums[k + i - 1] / k
-            averages.append(cur_avg)
-        return max(averages)
+            cur_avg += nums[k + i - 1] - nums[i - 1] 
+            if cur_avg > max_avg:
+                max_avg = cur_avg
+        return max_avg / k
