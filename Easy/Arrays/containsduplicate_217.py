9f96b3350160993bdf451e9c8994e6dfec162863
Max Shabs <97040079+maxshabs@users.noreply.github.com>
2024-12-02 00:05:23 +0200

    Update problem 217

--- Easy/Arrays/containsduplicate_217.py
+++ Easy/Arrays/containsduplicate_217.py
@@ -1,9 +1,11 @@
+# Solution for LeetCode Problem 217. Contains Duplicate
+# Time Complexity: O(n), where n is the size of the input array "nums"
+# Space Complexity: O(n), because the worst case is that all of the numbers are in the set
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums_dict = {}
+        nums_set = set()
         for num in nums:
-            if num in nums_dict:
+            if num in nums_set:
                 return True
-            else:
-                nums_dict[num] = 1
+            nums_set.add(num)
         return False
